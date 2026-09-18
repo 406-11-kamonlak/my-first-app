5d8014d9b1d9b48d8fef3186335fba3afdb1271e
406-11-kamonlak <54296@yupparaj.ac.th>
2026-09-18 22:10:57 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -66,7 +66,7 @@ def show_result_dialog(ans1, ans2):
 
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
-    if score == 4:
+    if score == 2:
         st.success("🎉 You win!")
     else:
         st.error("💀 You lose!")
