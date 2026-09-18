221cf3c822895a5ce5cd28bac9754d73d8f68f1d
406-11-kamonlak <54296@yupparaj.ac.th>
2026-09-18 22:07:46 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -50,6 +50,8 @@ def show_result_dialog(ans1, ans2):
     else:
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
         
+  # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4 ตรงนี้
+      
     if u_ans3 == "book":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
@@ -65,7 +67,7 @@ def show_result_dialog(ans1, ans2):
 
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
-    if score == 2:
+    if score == 4:
         st.success("🎉 You win!")
     else:
         st.error("💀 You lose!")
@@ -124,7 +126,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2)
+    show_result_dialog(ans1, ans2, ans3, ans4)
 
 st.divider()
 st.write("นางสาวกมลลักษณ์ แซ่ตวน เลขที่ 11 ม.4/6")
