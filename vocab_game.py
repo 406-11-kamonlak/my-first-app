cd7dcc5065aee53b1cb701ed6a82608afde86a9b
406-11-kamonlak <54296@yupparaj.ac.th>
2026-09-18 22:00:37 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -100,11 +100,11 @@ ans2 = st.text_input(
     value=st.session_state.ans2_val,
 )
 ans3 = st.text_input(
-    "ข้อ 3: Cats love to eat `b_ok`. 🐟",
+    "ข้อ 3: Cats love to eat `b_ok`. 📚",
     value=st.session_state.ans3_val,
 )
 ans4 = st.text_input(
-    "ข้อ 4: Cats love to eat `lem_n`. 🐟",
+    "ข้อ 4: Cats love to eat `lem_n`. 🍋",
     value=st.session_state.ans4_val,
 )
 
